bd241d423c697bd1a8761e37a990124c4666f20b
Luke Braby <lbraby0314@gmail.com>
2024-04-22 01:18:17 -0400

    tables accessible through /admin

--- benchmarking_project/benchmarks/admin.py
+++ benchmarking_project/benchmarks/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Topology, Manufacturer, Technology, Processor
+from .models import GateSet, Gate, GateSetMembership
+from .models import System, Calibration
+from .models import EmbeddingAlgorithm, Solver, TimeType, PerformanceMetric, Graph, Problem, PerformanceReport
 
 # Register your models here.
+admin.site.register(Topology)
+admin.site.register(Manufacturer)
+admin.site.register(Technology)
+admin.site.register(Processor)
+admin.site.register(GateSet)
+admin.site.register(Gate)
+admin.site.register(GateSetMembership)
+admin.site.register(System)
+admin.site.register(Calibration)
+admin.site.register(EmbeddingAlgorithm)
+admin.site.register(Solver)
+admin.site.register(TimeType)
+admin.site.register(PerformanceMetric)
+admin.site.register(Graph)
+admin.site.register(Problem)
+admin.site.register(PerformanceReport)
--- benchmarking_project/benchmarks/models.py
+++ benchmarking_project/benchmarks/models.py
@@ -1,151 +1,184 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
-class Problem(models.Model):
-    name = models.CharField(max_length=50)
-    graph_size = models.IntegerField
-    graph_type = models.CharField(max_length=100)
-    url1 = models.URLField(max_length=200)
-    url2 = models.URLField(max_length=200)
-    notes = models.TextField
-
-    def __str__(self):
-        return f"{self.name} (size={self.graph_size}, type={self.graph_type})"
-    
+# Create your models here.  
 class Topology(models.Model):
-    name = models.CharField(max_length=50)
-    physical_qubits_per_cell = models.IntegerField
-    qubit_degree = models.IntegerField
-    qubit_nominal_length = models.IntegerField
-    max_qubo_variable_count_clique = models.IntegerField
-    url1 = models.URLField(max_length=200)
-    url2 = models.URLField(max_length=200)
-    notes = models.TextField
+    name = models.CharField(max_length=50, null=False, blank=False)
+    physical_qubits_per_cell = models.IntegerField(null=False, blank=False)
+    qubit_degree = models.FloatField(null=False, blank=False)
+    qubit_nominal_length = models.IntegerField(null=True, blank=True, default=None)
+    max_qubo_variable_count_clique = models.IntegerField(null=True, blank=True, default=None)
+    url1 = models.URLField(max_length=200, blank=True)
+    url2 = models.URLField(max_length=200, blank=True)
+    notes = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
 
 class Manufacturer(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+    
+class Technology(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
 
     def __str__(self):
         return self.name
 
-# TODO: decide which fields belong to processor and which belong to system
 class Processor(models.Model):
-    name = models.CharField(max_length=50)
-    manufacturer_id = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    physical_qubits = models.IntegerField
-    intro_year = models.IntegerField
-    topology_id = models.ForeignKey(Topology, on_delete=models.CASCADE)
-    # rep_rate = models.
-    
-class Technology(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    technology_id = models.ForeignKey(Technology, on_delete=models.CASCADE, null=False, blank=False)
+    manufacturer_id = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, null=False, blank=False)
+    physical_qubits = models.IntegerField(null=True, blank=True, default=None)
+    topology_id = models.ForeignKey(Topology, on_delete=models.CASCADE, null=True, blank=True, default=None)
+    intro_year = models.IntegerField(null=True, blank=True, default=None)
+    rep_rate = models.FloatField(null=True, blank=True, default=None)
 
     def __str__(self):
         return self.name
     
 class GateSet(models.Model):
-    name = models.CharField(max_length=50)
-    url1 = models.URLField(max_length=200)
-    url2 = models.URLField(max_length=200)
-    notes = models.TextField
+    name = models.CharField(max_length=50, null=False, blank=False)
+    url1 = models.URLField(max_length=200, blank=True)
+    url2 = models.URLField(max_length=200, blank=True)
+    notes = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
     
 class Gate(models.Model):
-    name = models.CharField(max_length=50)
-    qubits = models.IntegerField
-    is_virtual = models.BooleanField
-    url1 = models.URLField(max_length=200)
-    url2 = models.URLField(max_length=200)
-    notes = models.TextField
+    name = models.CharField(max_length=50, null=False, blank=False)
+    qubits = models.IntegerField(null=False, blank=False)
+    url1 = models.URLField(max_length=200, blank=True)
+    url2 = models.URLField(max_length=200, blank=True)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.qubits}Q {self.name}"
+    
+class GateSetMembership(models.Model):
+    gate_set_id = models.ForeignKey(GateSet, on_delete=models.CASCADE, null=False, blank=False)
+    gate_id = models.ForeignKey(Gate, on_delete=models.CASCADE, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.gate_set_id.name} - {self.gate_id.qubits}Q {self.gate_id.name}"
+
+class System(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    manufacturer_id = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, null=False, blank=False)
+    processor_id = models.ForeignKey(Processor, on_delete=models.CASCADE, null=False, blank=False)
+    intro_year = models.IntegerField(null=True, blank=True, default=None)
+    gate_set_id = models.ForeignKey(GateSet, on_delete=models.CASCADE, null=True, blank=True, default=None)
+    url1 = models.URLField(max_length=200, blank=True)
+    url2 = models.URLField(max_length=200, blank=True)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Calibration(models.Model):
+    system_id = models.ForeignKey(System, on_delete=models.CASCADE, null=False, blank=False)
+    date = models.DateTimeField(null=True, blank=True, default=None)
+    eplg = models.FloatField(null=True, blank=True, default=None)
+    clops = models.IntegerField(null=True, blank=True, default=None)
+    median_cz_err = models.FloatField(null=True, blank=True, default=None)
+    median_ecr_err = models.FloatField(null=True, blank=True, default=None)
+    median_cnot_err = models.FloatField(null=True, blank=True, default=None)
+    median_sx_err = models.FloatField(null=True, blank=True, default=None)
+    min_1q_err = models.FloatField(null=True, blank=True, default=None)
+    max_1q_err = models.FloatField(null=True, blank=True, default=None)
+    typical_1q_err = models.FloatField(null=True, blank=True, default=None)
+    median_1q_err = models.FloatField(null=True, blank=True, default=None)
+    min_2q_err = models.FloatField(null=True, blank=True, default=None)
+    max_2q_err = models.FloatField(null=True, blank=True, default=None)
+    typical_2q_err = models.FloatField(null=True, blank=True, default=None)
+    median_2q_err = models.FloatField(null=True, blank=True, default=None)
+    median_readout_err = models.FloatField(null=True, blank=True, default=None)
+    spam_err = models.FloatField(null=True, blank=True, default=None)
+    mem_err_avg_d1_circuit = models.FloatField(null=True, blank=True, default=None)
+    crosstalk_err_mid_circuit = models.FloatField(null=True, blank=True, default=None)
+    min_T1 = models.FloatField(null=True, blank=True, default=None)
+    max_T1 = models.FloatField(null=True, blank=True, default=None)
+    median_T1 = models.FloatField(null=True, blank=True, default=None)
+    mean_T1 = models.FloatField(null=True, blank=True, default=None)
+    min_T2 = models.FloatField(null=True, blank=True, default=None)
+    max_T2 = models.FloatField(null=True, blank=True, default=None)
+    median_T2 = models.FloatField(null=True, blank=True, default=None)
+    mean_T2 = models.FloatField(null=True, blank=True, default=None)
+    url1 = models.URLField(max_length=200, blank=True)
+    url2 = models.URLField(max_length=200, blank=True)
+    notes = models.TextField(blank=True)
+
+class EmbeddingAlgorithm(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
 
     def __str__(self):
         return self.name
     
-class GateSetMembership(models.Model):
-    gate_set_id = models.ForeignKey(GateSet, on_delete=models.CASCADE)
-    gate_id = models.ForeignKey(Gate, on_delete=models.CASCADE)
-
-class System(models.Model):
-    system_name = models.CharField(max_length=50)
-    manufacturer_id = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    technology_id = models.ForeignKey(Technology, on_delete=models.CASCADE)
-    physical_qubit_count = models.IntegerField
-    processor_id = models.ForeignKey(Processor, on_delete=models.CASCADE)
-    intro_year = models.IntegerField
-    topology_id = models.ForeignKey(Topology, on_delete=models.CASCADE)
-    gate_set_id = models.ForeignKey(GateSet, on_delete=models.CASCADE)
-    url1 = models.URLField(max_length=200)
-    url2 = models.URLField(max_length=200)
-    notes = models.TextField
+class Solver(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
 
     def __str__(self):
-        return self.system_name
-
-class Calibration(models.Model):
-    system_id = models.ForeignKey(System, on_delete=models.CASCADE)
-    date = models.DateTimeField
-    eplg = models.FloatField
-    clops = models.IntegerField
-    median_cz_err = models.FloatField
-    median_ecr_err = models.FloatField
-    median_cnot_err = models.FloatField
-    median_sx_err = models.FloatField
-    min_1q_err = models.FloatField
-    max_1q_err = models.FloatField
-    typical_1q_err = models.FloatField
-    median_1q_err = models.FloatField
-    min_2q_err = models.FloatField
-    max_2q_err = models.FloatField
-    typical_2q_err = models.FloatField
-    median_2q_err = models.FloatField
-    median_readout_err = models.FloatField
-    spam_err = models.FloatField
-    mem_err_avg_d1_circuit = models.FloatField
-    crosstalk_err_mid_circuit = models.FloatField
-    min_T1 = models.FloatField
-    max_T1 = models.FloatField
-    median_T1 = models.FloatField
-    mean_T1 = models.FloatField
-    min_T2 = models.FloatField
-    max_T2 = models.FloatField
-    median_T2 = models.FloatField
-    mean_T2 = models.FloatField
-    url1 = models.URLField(max_length=200)
-    url2 = models.URLField(max_length=200)
-    notes = models.TextField
+        return self.name
 
 class TimeType(models.Model):
-    time_type = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, null=False, blank=False)
 
     def __str__(self):
-        return self.time_type
+        return self.name
 
 class PerformanceMetric(models.Model):
-    performance_metric = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, null=False, blank=False)
 
     def __str__(self):
-        return self.performance_metric
+        return self.name
+    
+class Graph(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    url1 = models.URLField(max_length=200, blank=True)
+    url2 = models.URLField(max_length=200, blank=True)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Problem(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    graph_size = models.IntegerField(null=False, blank=False)
+    graph_id = models.ForeignKey(Graph, on_delete=models.CASCADE, null=False, blank=False)
+    url1 = models.URLField(max_length=200, blank=True)
+    url2 = models.URLField(max_length=200, blank=True)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.name} (size={self.graph_size}, type={self.graph_id.name})"
 
 class PerformanceReport(models.Model):
-    problem_id = models.ForeignKey(Problem, on_delete=models.CASCADE)
-    qubo_var_count = models.IntegerField
-    qubo_quad_term_count = models.IntegerField
-    system_id = models.ForeignKey(System, on_delete=models.CASCADE)
-    embedding_alg = models.CharField(max_length=50)
-    solver = models.CharField(max_length=50)
-    qubit_count = models.IntegerField
-    rcs = models.FloatField
-    mean_chain_length = models.IntegerField
-    max_chain_length = models.IntegerField
-    num_runs = models.IntegerField
-    type_type_id = models.ForeignKey(System, on_delete=models.CASCADE)
-    time = models.FloatField
-    performance_metric_id = models.ForeignKey(PerformanceMetric, on_delete=models.CASCADE)
-    performance_value = models.FloatField
-    notes = models.TextField
+    problem_id = models.ForeignKey(Problem, on_delete=models.CASCADE, null=False, blank=False)
+    qubo_var_count = models.IntegerField(null=True, blank=True, default=None)
+    qubo_quad_term_count = models.IntegerField(null=True, blank=True, default=None)
+    system_id = models.ForeignKey(System, on_delete=models.CASCADE, null=True, blank=True, default=None)
+    embedding_id = models.ForeignKey(EmbeddingAlgorithm, on_delete=models.CASCADE, null=True, blank=True, default=None)
+    solver_id = models.ForeignKey(Solver, on_delete=models.CASCADE, null=True, blank=True, default=None)
+    qubit_count = models.IntegerField(null=True, blank=True, default=None)
+    rcs = models.FloatField(null=True, blank=True, default=None)
+    mean_chain_length = models.IntegerField(null=True, blank=True, default=None)
+    max_chain_length = models.IntegerField(null=True, blank=True, default=None)
+    num_runs = models.IntegerField(null=True, blank=True, default=None)
+    time_type_id = models.ForeignKey(TimeType, on_delete=models.CASCADE, null=True, blank=True, default=None)
+    time = models.FloatField(null=True, blank=True, default=None)
+    performance_metric_id = models.ForeignKey(PerformanceMetric, on_delete=models.CASCADE, null=True, blank=True, default=None)
+    performance_value = models.FloatField(null=True, blank=True, default=None)
+    url1 = models.URLField(max_length=200, blank=True)
+    url2 = models.URLField(max_length=200, blank=True)
+    notes = models.TextField(null=True, blank=True, default=None)
 
+    def clean(self):
+        super().clean()
+        if self.time is not None and self.time_type_id is None:
+            raise ValidationError({'time_type_id': 'Time type must be specified if time is to be provided.'})
+        if self.performance_value is not None and self.performance_metric_id is None:
+            raise ValidationError({'performance_metric_id': 'Performance metric type must be specified if performance value is to be provided.'})
+    
